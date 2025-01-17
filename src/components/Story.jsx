4b4ebb58bec33afc0b4affe0a653953fb4f4fec1
Cbreezz <ngigibrown@gmail.com>
2025-01-18 02:20:01 +0300

    updates

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -106,8 +106,8 @@ const FloatingImage = () => {
         <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
           <div className="flex h-full w-fit flex-col items-center md:items-start">
             <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
-              In the realm where anime realms converge, lies Zentry and the boundless pillar. 
-              Discover its secrets and shape your destiny amidst infinite possibilities.
+              Tell us your favorite anime and why and when you started watching Anime. 
+              Be a part of an amazing Community and build the cultture.
             </p>
 
             <Button
